9e3361e5e6cc7cd4c8af00eb812cdeab57662a16
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-07-12 20:58:51 +0900

    fix: view model, state 불러와서 데이터 뿌려줌

--- lib/ui/main/main_screen.dart
+++ lib/ui/main/main_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:image_search_9day/ui/main/main_view_model.dart';
+import 'package:provider/provider.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -18,6 +21,9 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final viewModel = context.watch<MainViewModel>();
+    final state = viewModel.state;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('이미지 검색 앱'),
@@ -29,9 +35,39 @@ class _MainScreenState extends State<MainScreen> {
             decoration: InputDecoration(
               hintText: '이미지를 검색해보세요',
               suffixIcon: IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  viewModel.fetchImages(controller.text);
+                },
                 icon: const Icon(Icons.search),
               ),
+            ),
+          ),
+          Expanded(
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 4,
+                crossAxisSpacing: 4,
+              ),
+              itemCount: state.photos.length,
+              itemBuilder: (context, index) {
+                final photo = state.photos[index];
+                return Hero(
+                  tag: photo.id,
+                  child: GestureDetector(
+                    onTap: () {
+                      context.push(
+                        '/detail',
+                        extra: photo,
+                      );
+                    },
+                    child: Image.network(
+                      photo.url,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                );
+              },
             ),
           )
         ],
