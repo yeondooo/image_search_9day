367dfdc9aa86b328709df8dd3e321e7f47306b16
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-07-12 20:47:57 +0900

    feat: main state 생성

---
+++ lib/ui/main/main_state.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+import 'package:image_search_9day/data/model/photo.dart';
+
+part 'main_state.freezed.dart';
+
+part 'main_state.g.dart';
+
+@freezed
+class MainState with _$MainState {
+  const factory MainState({
+    @Default([]) List<Photo> photos,
+    @Default(false) bool isLoading,
+  }) = _MainState;
+
+  factory MainState.fromJson(Map<String, Object?> json) =>
+      _$MainStateFromJson(json);
+}
