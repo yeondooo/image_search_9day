f180ca4629db9570b6c3ed8785328a1e953e744e
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-07-12 20:18:36 +0900

    feat: photo model class 구현

---
+++ lib/data/model/photo.dart
@@ -0,0 +1,20 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'photo.g.dart';
+
+@JsonSerializable(explicitToJson: true)
+class Photo {
+  final num id;
+  final String url;
+  final num views;
+
+  Photo({
+    required this.id,
+    required this.url,
+    required this.views,
+  });
+
+  factory Photo.fromJson(Map<String, dynamic> json) => _$PhotoFromJson(json);
+
+  Map<String, dynamic> toJson() => _$PhotoToJson(this);
+}
