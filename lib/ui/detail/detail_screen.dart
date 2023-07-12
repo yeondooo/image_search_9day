7c57cae7b0d8b56e5494dde27030978d1e2e47bb
yeonju <97602748+yeondooo@users.noreply.github.com>
2023-07-12 20:31:05 +0900

    feat: detail screen ui 구현

---
+++ lib/ui/detail/detail_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:image_search_9day/data/model/photo.dart';
+
+class DetailScreen extends StatelessWidget {
+  final Photo photo;
+
+  const DetailScreen({Key? key, required this.photo}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('디테일 페이지'),
+      ),
+      body: Image.network('/'),
+    );
+  }
+}
